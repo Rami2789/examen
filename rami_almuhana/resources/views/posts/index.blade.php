916ac691fb4108d808ebabaab599c92143e464d3
Rami <74742680+Rami2789@users.noreply.github.com>
2023-01-23 10:40:42 +0100

    Nu kun je de posts zien als je een post selecteert

--- rami_almuhana/resources/views/posts/index.blade.php
+++ rami_almuhana/resources/views/posts/index.blade.php
@@ -7,13 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <a href="{{ route('posts.create') }}" class="btn-link btn-lg mb-2">+ New Note</a>
+        <a href="{{ route('posts.create') }}" class="btn-link btn-lg mb-2">+ New Post</a>
         @foreach ($posts as $post)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h1 class="font-bold text-2xl">
-                        {{ $post->author }}
+                        <a href="{{ route('posts.show', $post) }}">{{ $post->author }}</a>
                     </h1>
-                    <h2 class="font-bold text-2x1">
+
+                    <h2 class="font-bold text-2x">
                         {{ $post->title }}
                     </h2>
                     <p class="mt-2">
---
+++ rami_almuhana/resources/views/posts/show.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <div class="flex">
+                <p class="opacity-70">
+                   <strong>Created: </strong> {{ $post->created_at->diffForHumans() }}
+                </p>
+                <p class="opacity-70 ml-8">
+                <strong>Updated at: </strong> {{ $post->updated_at->diffForHumans() }}
+                </p>
+            </div>
+
+
+<div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                <h1 class="font-bold text-3xl">
+                    {{ $post->author }}
+                </h1>
+                <h2 class="font-bold text-2xl">
+                    {{ $post->title }}
+                </h2>
+                <p class="mt-6 whitespace-pre-wrap">
+                    {{ $post->text }}
+                </p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
