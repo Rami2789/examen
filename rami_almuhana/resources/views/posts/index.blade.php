cfbdff1fac8f636788ac891abbb8d39b7f4259ac
Rami <74742680+Rami2789@users.noreply.github.com>
2023-01-23 10:20:32 +0100

    Pagina aangemaakt voor de create met een formulier

--- rami_almuhana/resources/views/posts/index.blade.php
+++ rami_almuhana/resources/views/posts/index.blade.php
@@ -7,6 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <a href="{{ route('posts.create') }}" class="btn-link btn-lg mb-2">+ New Note</a>
         @foreach ($posts as $post)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h1 class="font-bold text-2xl">
